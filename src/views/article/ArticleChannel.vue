2fe649ee13e9099341b9ff6a9be26c6d311267d5
ericheng0124 <233347237@qq.com>
2024-11-22 10:22:10 +0800

    创建弹层组件，并暴漏open方法实现添加和编辑

--- src/views/article/ArticleChannel.vue
+++ src/views/article/ArticleChannel.vue
@@ -2,29 +2,38 @@
 import { artGetChannelsService } from '@/api/article'
 import { ref } from 'vue'
 import { Edit, Delete } from '@element-plus/icons-vue'
+import ChannelEdit from './components/ChannelEdit.vue'
 
 const channelList = ref([])
-const loading = ref(false)
+const loading = ref(false) // 用于配置loading效果
+const dialog = ref()
+
 const getChannelList = async () => {
   loading.value = true
   const res = await artGetChannelsService()
   channelList.value = res.data.data
   loading.value = false
 }
+
 getChannelList()
 
 const onEditChannel = (row, $index) => {
   console.log(row, $index)
+  dialog.value.open(row)
 }
 
 const onDelChannel = (row, $index) => {
   console.log(row, $index)
 }
+
+const onAddChannel = () => {
+  dialog.value.open({})
+}
 </script>
 <template>
   <page-container title="文章分类">
     <template #extra>
-      <el-button type="primary">添加分类</el-button>
+      <el-button type="primary" @click="onAddChannel">添加分类</el-button>
     </template>
     <!-- 主体部分是 表格 -->
     <el-table v-loading="loading" :data="channelList" style="width: 100%">
@@ -54,10 +63,12 @@ const onDelChannel = (row, $index) => {
           </el-tooltip>
         </template>
       </el-table-column>
+      <!-- 配置空数据效果 -->
       <template #empty>
         <el-empty description="没有数据"></el-empty>
       </template>
     </el-table>
+    <ChannelEdit ref="dialog"></ChannelEdit>
   </page-container>
 </template>
 <style lang="scss" scoped></style>
---
+++ src/views/article/components/ChannelEdit.vue
@@ -0,0 +1,32 @@
+<script setup>
+import { ref } from 'vue'
+
+const dialogVisible = ref(false)
+
+// 组件对外暴漏一个方法 open ，基于open传来的参数，区分是添加操作还是编辑操作
+// open({}) => 传来的是个空对象，说明是添加操作
+// open({id,cate_name,...}) => 传来的有具体数据，表单需要渲染，说明是编辑操作
+// open调用后，可以打开弹层
+
+const open = (row) => {
+  console.log(row)
+  dialogVisible.value = true
+}
+
+// 向外暴漏方法
+defineExpose({
+  open
+})
+</script>
+
+<template>
+  <el-dialog v-model="dialogVisible" title="提示" width="500">
+    <span>弹层内容部分</span>
+    <template #footer>
+      <div class="dialog-footer">
+        <el-button @click="dialogVisible = false">取消</el-button>
+        <el-button type="primary" @click="dialogVisible = false">确认</el-button>
+      </div>
+    </template>
+  </el-dialog>
+</template>
