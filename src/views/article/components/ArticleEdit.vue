eff2c07ad40afa9ba1471f4f79a51fb67f4eb77b
ericheng0124 <233347237@qq.com>
2024-11-25 14:54:35 +0800

    文章图片上传的预览功能

--- src/views/article/components/ArticleEdit.vue
+++ src/views/article/components/ArticleEdit.vue
@@ -1,6 +1,7 @@
 <script setup>
 import { ref } from 'vue'
 import ChannelSelect from './ChannelSelect.vue'
+import { Plus } from '@element-plus/icons-vue'
 // 控制抽屉弹层的显示和隐藏
 const visibleDrawer = ref(false)
 
@@ -17,6 +18,14 @@ const defaultForm = {
 const formModel = ref({
   ...defaultForm
 })
+
+// 图片上传相关
+const imgUrl = ref('')
+
+const onSelectFile = (uploadFile) => {
+  // console.log(uploadFile)
+  imgUrl.value = URL.createObjectURL(uploadFile.raw) // 预览图片
+}
 
 // 组件对外暴漏一个方法 open ，基于open传来的参数，区分是添加操作还是编辑操作
 // open({}) => 传来的是个空对象，说明是添加操作
@@ -61,12 +70,27 @@ defineExpose({
     <!-- 发表文章表单 -->
     <el-form :model="formModel" ref="formRef" label-width="100px">
       <el-form-item label="文章标题" prop="title">
-        <el-input style="width: 80%" v-model="formModel.title" placeholder="请输入标题"></el-input>
+        <el-input style="width: 90%" v-model="formModel.title" placeholder="请输入标题"></el-input>
       </el-form-item>
       <el-form-item label="文章分类" prop="cate_id">
-        <channel-select v-model="formModel.cate_id" width="80%"></channel-select>
+        <channel-select v-model="formModel.cate_id" width="90%"></channel-select>
       </el-form-item>
-      <el-form-item label="文章封面" prop="cover_img"> 文件上传 </el-form-item>
+      <el-form-item label="文章封面" prop="cover_img">
+        <!-- 
+          此处需要关闭 element-plus 的自动上传，不需要配置 action 等参数
+          只需要做前端的本地预览图片即可，无需在提交之前上传图片
+          语法：URL.createObjectURL(...)创建本地预览地址，来预览
+        -->
+        <el-upload
+          class="avatar-uploader"
+          :show-file-list="false"
+          :auto-upload="false"
+          :on-change="onSelectFile"
+        >
+          <img v-if="imgUrl" :src="imgUrl" class="avatar" />
+          <el-icon v-else class="avatar-uploader-icon"><Plus /></el-icon>
+        </el-upload>
+      </el-form-item>
       <el-form-item label="文章内容" prop="content">
         <div class="editor">富文本编辑器</div>
       </el-form-item>
@@ -77,3 +101,33 @@ defineExpose({
     </el-form>
   </el-drawer>
 </template>
+
+<style lang="scss" scoped>
+.avatar-uploader {
+  :deep() {
+    .avatar {
+      width: 178px;
+      height: 178px;
+      display: block;
+    }
+    .el-upload {
+      border: 1px dashed var(--el-border-color);
+      border-radius: 6px;
+      cursor: pointer;
+      position: relative;
+      overflow: hidden;
+      transition: var(--el-transition-duration-fast);
+    }
+    .el-upload:hover {
+      border-color: var(--el-color-primary);
+    }
+    .el-icon.avatar-uploader-icon {
+      font-size: 28px;
+      color: #8c939d;
+      width: 178px;
+      height: 178px;
+      text-align: center;
+    }
+  }
+}
+</style>
